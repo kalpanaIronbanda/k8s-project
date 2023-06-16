2c08ca737d5cfe4b28c036f7417c799ed071fa91
kalpana <kalpanaironbanda@gmail.com>
2023-06-16 09:39:01 +0530

    deploy

--- micro-svc-backend/app.py
+++ micro-svc-backend/app.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from flask import Flask, render_template
 import os
+import logging
 
 app = Flask(__name__)
 
@@ -22,6 +23,33 @@ password = os.getenv('DB_PASSWORD')
 
 # Read table name from environment variable
 table_name = os.getenv('TABLE_NAME', 'default-table')
+
+# Check if values are from ConfigMap or Secret
+if password is None:
+    logging.warning("Database password not found in Secret!")
+else:
+    logging.info("Database password retrieved from Secret.")
+
+if user == 'default-user':
+    logging.warning("Database user not found in ConfigMap!")
+else:
+    logging.info("Database user retrieved from ConfigMap.")
+
+if host == 'default-host':
+    logging.warning("Database host not found in ConfigMap!")
+else:
+    logging.info("Database host retrieved from ConfigMap.")
+
+if database == 'default-database':
+    logging.warning("Database name not found in ConfigMap!")
+else:
+    logging.info("Database name retrieved from ConfigMap.")
+
+if table_name == 'default-table':
+    logging.warning("Table name not found in environment variable!")
+else:
+    logging.info("Table name retrieved from environment variable.")
+
 
 @app.route('/')
 def index():
