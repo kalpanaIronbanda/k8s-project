e64a21885b9ea27bd1dd4abec6e25187e630bd7c
kalpana <kalpanaironbanda@gmail.com>
2023-06-16 11:34:44 +0530

    fixed the bug

--- micro-svc-backend/db_connector.py
+++ micro-svc-backend/db_connector.py
@@ -1,4 +1,4 @@
-import psycopg2
+import mysql.connector
 import os
 from dotenv import load_dotenv
 
@@ -8,17 +8,13 @@ load_dotenv()  # Loading .env file for development
 class DbConnector:
     def connect(self):
         try:
-            conn = psycopg2.connect(
+            conn = mysql.connector.connect(
                 host=os.getenv("DB_HOST"),
                 database=os.getenv("DB_NAME"),
                 user=os.getenv("DB_USER"),
                 password=os.getenv("DB_PASSWORD"),
-                table_name=os.getenv("TABLE_NAME"),
-                sslmode="prefer"
-                if os.getenv("DB_DISABLE_SSL_MODE") == "true"
-                else "require",
             )
             print("Connected to the database successfully!!")
             return conn
-        except psycopg2.Error as e:
+        except mysql.connector.Error as e:
             raise Exception("Unable to connect to database: {}".format(e))
